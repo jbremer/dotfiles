a72294f691cdb483f2909e9d3361e9670c67824e
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-12-06 18:58:52 +0100

    added vimfiles, including asu1dark color theme

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Installation
 ============
 
 Install Pathogen.
-Download asu1dark theme
 Download Syntastic
 http://www.vim.org/scripts/script.php?script\_id=386
 http://www.vim.org/scripts/script.php?script\_id=974
--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 ln -s $PWD/tmux-conf ~/.tmux.conf
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/bash-aliases ~/.bash_aliases
+
+if [ "$OS" == "Windows_NT" ]; then
+    cp -rf $PWD/vimfiles ~
+else
+    ln -s $PWD/vimfiles ~/.vim
+fi
