5a554d993b52bdcfac29ec41c84c3fb22f9fadd7
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-12-08 00:31:27 +0100

    use local variables rather than aliases

--- setup.sh
+++ setup.sh
@@ -1,27 +1,26 @@
-
 if [ "$OS" == "Windows_NT" ]; then
-    alias _assign_dir='cp -rf'
-    alias _assign_file='cp -f'
+    assign_dir='cp -rf'
+    assign_file='cp -f'
     echo '[x] Windows detected'
 else
-    alias _assign_dir='ln -s'
-    alias _assign_file='ln -s'
+    assign_dir='ln -s'
+    assign_file='ln -s'
     echo '[x] Linux detected'
 
     echo '[+] dropping .tmux.conf'
-    _assign_file "$PWD/tmux-conf" ~/.tmux.conf
+    $assign_file "$PWD/tmux-conf" ~/.tmux.conf
 fi
 
 echo '[+] dropping .vimrc'
-_assign_file "$PWD/vimrc" ~/.vimrc
+$assign_file "$PWD/vimrc" ~/.vimrc
 
 if [ ! -f ~/.bashrc ]; then
     echo '[+] no .bashrc found, dropping one now'
-    _assign_file "$PWD/bashrc" ~/.bashrc
+    $assign_file "$PWD/bashrc" ~/.bashrc
 fi
 
 echo '[+] dropping .bash_aliases'
-_assign_file "$PWD/bash-aliases" ~/.bash_aliases
+$assign_file "$PWD/bash-aliases" ~/.bash_aliases
 
 echo '[+] installing vim-pathogen'
 mkdir -p "$PWD/vimfiles/autoload" "$PWD/vimfiles/bundle"
@@ -41,12 +40,9 @@ git submodule foreach git submodule update
 echo '[x] dropping all vimfiles'
 
 if [ "$OS" == "Windows_NT" ]; then
-    _assign_dir "$PWD/vimfiles" ~
+    $assign_dir "$PWD/vimfiles" ~
 else
-    _assign_dir "$PWD/vimfiles" ~/.vim
+    $assign_dir "$PWD/vimfiles" ~/.vim
 fi
-
-unalias _assign_file
-unalias _assign_dir
 
 echo '[x] initialized successfully'
